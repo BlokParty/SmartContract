fbe72b912369ad33c994b28d51921107b7932248
Sherman <sherman_meredith@hotmail.com>
2018-08-16 17:57:33 -0700

    Thursday update
    Fixed Detailed View
    Added Description, Deposited token fields

--- BlockChain Reader/Assets/Scripts/Inventory.cs
+++ BlockChain Reader/Assets/Scripts/Inventory.cs
@@ -5,12 +5,31 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
-    uint numberOfOwnedAacs;
+    public uint numberOfOwnedAacs;
     AacContractReader.GetAacDto[] ownedAacs;
     public AacContractReader.GetAacDto GetOwnedAac(uint index) { return ownedAacs[index]; }
     public void SetOwnedAac(uint index, AacContractReader.GetAacDto value) { ownedAacs[index] = value; }
+
+    [System.Serializable]
+    public class Slot
+    {
+        Image image;
+        public Image MyImage { get { return image; } set { image = value; } }
+        string age;
+        public Text Age { get; set; }
+        string type;
+        public Text Type { get; set; }
+        string name;
+        public Text Name { get; set; }
+        string exp;
+        public Text Exp { get; set; }
+        string value;
+        public Text Value { get; set; }
+        string favorite;
+        public Text Favorite { get; set; }
+    }
     [SerializeField]
-    Image[] images;
+    Slot[] slots;
     string url = "https://www.blok.party/aacs/";
 
     int viewingIndex;
@@ -33,7 +52,6 @@ public class Inventory : MonoBehaviour {
     [SerializeField]
     Sprite Charizard;
 
-
     public void OnFinishedLoading()
     {
         for(int i = 0; i < ownedAacs.Length; ++i)
@@ -46,18 +64,34 @@ public class Inventory : MonoBehaviour {
             if(ownedAacs[i].UID > 0xFFFFFFFFFFFFFF)
             {
                 // empty AAC
-                images[i].sprite = EmptyAacSprite;
-                images[i].enabled = true;
+                slots[i].MyImage.sprite = EmptyAacSprite;
+                slots[i].MyImage.enabled = true;
+                slots[i].Age.text = "N/A";
+                slots[i].Type.text = "Empty";
+                slots[i].Name.text = "N/A";
+                slots[i].Exp.text = "0";
+                slots[i].Value.text = "0";
+                slots[i].Favorite.text = "";
             }
             else if(i == 0)
             {
-                images[i].sprite = Charizard;
-                images[i].enabled = true;
+                slots[i].MyImage.sprite = Charizard;
+                slots[i].MyImage.enabled = true;
+                slots[i].Age.text = GenerateAgeString(ownedAacs[i].Timestamp);
+                slots[i].Type.text = ownedAacs[i].UID.ToString("X14").Substring(0, 2);
+                slots[i].Name.text = "";
+                slots[i].Exp.text = ownedAacs[i].Experience.ToString();
+                slots[i].Favorite.text = "";
             }
             else
             {
-                images[i].sprite = defaultSprite;
-                images[i].enabled = true;
+                slots[i].MyImage.sprite = defaultSprite;
+                slots[i].MyImage.enabled = true;
+                slots[i].Age.text = GenerateAgeString(ownedAacs[i].Timestamp);
+                slots[i].Type.text = ownedAacs[i].UID.ToString("X14").Substring(0,2);
+                slots[i].Name.text = "";
+                slots[i].Exp.text = ownedAacs[i].Experience.ToString();
+                slots[i].Favorite.text = "";
             }
         }
         if(ownedAacs.Length > 0)
@@ -86,44 +120,77 @@ public class Inventory : MonoBehaviour {
             aacExp.text = "Exp:  0";
         }
         viewingIndex = index;
-        viewingAac.sprite = images[index].sprite;
+        viewingAac.sprite = slots[index].MyImage.sprite;
+        print(index);
     }
 
     public void InitializeOwnedAacs(uint length)
     {
         numberOfOwnedAacs = length;
         ownedAacs = new AacContractReader.GetAacDto[length];
-        images = new Image[length];
+        slots = new Slot[length];
         for(int i = 0; i < length; ++i)
         {
-            if (i > 17)
-            {
-                GameObject newButton = Instantiate(inventoryButton, transform.position, transform.rotation) as GameObject;
-                newButton.name = " (" + i +")";
-                newButton.transform.parent = transform;
-                newButton.GetComponent<Button>().onClick.AddListener(delegate { DisplayAac(i); });
-            }
-            images[i] = transform.Find(" (" + i + ")").transform.Find("Image").GetComponent<Image>();
+            slots[i] = new Slot();
+            if (i > 17) { MakeNewButton(i); }
+            slots[i].MyImage = transform.Find(" (" + i + ")").transform.Find("Image").GetComponent<Image>();
+            slots[i].Age = transform.Find(" (" + i + ")").transform.Find("Age").transform.Find("Text").GetComponent<Text>();
+            slots[i].Type = transform.Find(" (" + i + ")").transform.Find("Type").transform.Find("Text").GetComponent<Text>();
+            slots[i].Name = transform.Find(" (" + i + ")").transform.Find("Name").transform.Find("Text").GetComponent<Text>();
+            slots[i].Exp = transform.Find(" (" + i + ")").transform.Find("Exp").transform.Find("Text").GetComponent<Text>();
+            slots[i].Value = transform.Find(" (" + i + ")").transform.Find("Value").transform.Find("Text").GetComponent<Text>();
+            slots[i].Favorite = transform.Find(" (" + i + ")").transform.Find("Favorite").transform.Find("Text").GetComponent<Text>();
             transform.Find(" (" + i + ")").GetComponent<Button>().enabled = true;
         }
     }
 
-    public string GenerateAgeString(uint timestamp)
+    public void SortByAge()
+    {
+        System.Array.Sort(ownedAacs, delegate (AacContractReader.GetAacDto a, AacContractReader.GetAacDto b)
+        {
+            return a.Timestamp.CompareTo(b.Timestamp);
+        });
+        OnFinishedLoading();
+        print("is this thing on?");
+    }
+
+    public void SortByType()
+    {
+        System.Array.Sort(ownedAacs, delegate (AacContractReader.GetAacDto a, AacContractReader.GetAacDto b)
+        {
+            return a.UID.CompareTo(b.UID);
+        });
+        OnFinishedLoading();
+        print("yes");
+    }
+
+    public void SortByXP()
+    {
+        System.Array.Sort(ownedAacs, delegate (AacContractReader.GetAacDto a, AacContractReader.GetAacDto b)
+        {
+            return a.Experience.CompareTo(b.Experience);
+        });
+        OnFinishedLoading();
+    }
+
+    private string GenerateAgeString(uint timestamp)
     {
         System.TimeSpan span = System.DateTime.Now.Subtract(new System.DateTime(1970, 1, 1, 0, 0, 0));
         uint age = (uint)span.TotalSeconds - timestamp;
         age /= 60;
-        if(age < 180)
-        {
-            return age + " minutes";
-        }
+        if(age < 180){ return age + " minutes"; }
         age /= 60;
-        if(age < 72)
-        {
-            return age + " hours";
-        }
+        if(age < 72){ return age + " hours"; }
         age /= 24;
-
         return age + " days";
+    }
+
+    private void MakeNewButton(int i)
+    {
+        GameObject newButton = Instantiate(inventoryButton, transform.position, transform.rotation) as GameObject;
+        newButton.name = " (" + i + ")";
+        newButton.transform.SetParent(transform);
+        newButton.transform.localScale = Vector3.one;
+        newButton.GetComponent<Button>().onClick.AddListener(delegate { DisplayAac(i); });
     }
 }
