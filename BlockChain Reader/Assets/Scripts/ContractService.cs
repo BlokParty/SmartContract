216cea21824eede87d508e14ae370b560e3fdb65
Sherman <sherman_meredith@hotmail.com>
2018-09-14 20:04:02 -0700

    bug fix
    images now pull from server

--- BlockChain Reader/Assets/Scripts/ContractService.cs
+++ BlockChain Reader/Assets/Scripts/ContractService.cs
@@ -140,7 +140,13 @@ public class ContractService : MonoBehaviour {
                 toyManager.toyTokens[index].Name = metadata.Name;
                 toyManager.toyTokens[index].Description = metadata.Description;
 
-                toyManager.toyTokens[index].Image = GenerateSpriteFromTexture2D(metadataRequest.Response.DataAsTexture2D);
+                metadataRequest = new HTTPRequest(new System.Uri(metadata.Image));
+                metadataRequest.Send();
+                yield return StartCoroutine(metadataRequest);
+                if (metadataRequest.Response != null)
+                {
+                    toyManager.toyTokens[index].Image = GenerateSpriteFromTexture2D(metadataRequest.Response.DataAsTexture2D);
+                }
             }
             else
             {
@@ -265,7 +271,11 @@ public class ContractService : MonoBehaviour {
 
     private Sprite GenerateSpriteFromTexture2D(Texture2D value)
     {
-        return Sprite.Create(value, new Rect(0, 0, value.width, value.height), new Vector2(0.5f, 0.5f));
+        if(value != null)
+        {
+            return Sprite.Create(value, new Rect(0, 0, value.width, value.height), new Vector2(0.5f, 0.5f));
+        }
+        return inventory.UnlinkedToySprite;
     }
 
     //-----------------------------------------------------------------------------------------------------------------
